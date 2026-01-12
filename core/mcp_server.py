958e286dfcbba33c15075303327bc4f02c2bd064
shavakkar <shavakkar@gmail.com>
2026-01-12 10:01:02 +0530

    good improvement :)

--- core/mcp_server.py
+++ core/mcp_server.py
@@ -11,16 +11,21 @@ Schema:
 - create_file requires: {"filename": "<string>", "content": "<string>"}
 - delete_file requires: {"filename": "<string>"}
 - read_file requires: {"filename": "<string>"}
+Some examples are given below.
 """
 
 DEVELOPER_PROMPT = """
 So, You are an assistant.
 Your responsibility is to interpret user requests and output the correct tool call.
 You must always choose one of the available tools and provide valid parameters.
+The user might ask for any files related to programming languages or any other file format that exists in the world.
 """
 
 FEW_SHOT_EXAMPLES = """
-Follow these instruction structure to assist, don't deviate.
+Follow these instruction structure as an example to assist and do the process, don't deviate.
+The below are some example way of creating the tool call, this resembles the Schema given.
+You should provide the very similar JSON structure by replacing the filename in according to the user's input.
+For creating the file, get the input accordingly and provide it to "content", if "none" leave it, if user askes to fill something, provide something relevant to the context of the user's input.
 
 User: create a file called hello.txt
 Assistant: {"tool": "create_file", "params": {"filename": "hello.txt", "content": ""}}
@@ -30,6 +35,8 @@ Assistant: {"tool": "delete_file", "params": {"filename": "hello.txt"}}
 
 User: read hello.txt
 Assistant: {"tool": "read_file", "params": {"filename": "hello.txt"}}
+
+Now, you will get the User's input. Try to follow all the above and return a JSON object like the above examples.
 """
 
 # def normalize_tool(tool_name):
@@ -56,12 +63,13 @@ def run_mcp_server(model, tokenizer):
 
         # Build prompt with system instruction
         prompt = SYSTEM_PROMPT + "\n" + FEW_SHOT_EXAMPLES + "\n" + DEVELOPER_PROMPT
-        prompt += "\nUser: " + user_input + "\nAssistant:"
+        prompt += "\nHere goes the User's input: " + user_input + "\nAssistant:"
+        # prompt += "\nHere goes the User's input: " + user_input + "\nAssistant:"
 
         inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
         outputs = model.generate(
             **inputs,
-            max_new_tokens=80,
+            max_new_tokens=60,
             pad_token_id=tokenizer.eos_token_id,
             eos_token_id=tokenizer.eos_token_id
         )
@@ -73,7 +81,7 @@ def run_mcp_server(model, tokenizer):
         match = re.findall(r'\{.*?\}', decoded, re.DOTALL)
         if match:
             try:
-                tool_call = json_repair.loads(match.group(0))
+                tool_call = json_repair.loads(match[-1])
                 tool_name = tool_call["tool"]
                 params = tool_call["params"]
                 response = route_tool_call(tool_name, params)
--- core/model_loader.py
+++ core/model_loader.py
@@ -5,7 +5,7 @@ def load_model(model_path="/models/deepseek-qwen-distill-1.5b"):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForCausalLM.from_pretrained(
         model_path,
-        dtype=torch.float16,
+        dtype=torch.float32,
         device_map="auto"   # auto-detect CPU/GPU
     )
     return model, tokenizer
